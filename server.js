32833d5fbee54d4796a1b064140acd9c255d1586
avig007 <105819973+avig007@users.noreply.github.com>
2025-05-24 00:33:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const googleCredentials = {
   type: process.env.GOOGLE_TYPE,
   project_id: process.env.GOOGLE_PROJECT_ID,
   private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-  private_key: "process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n')",
+  private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   client_email: "grievance@gen-lang-client-0458865392.iam.gserviceaccount.com",
   client_id: process.env.GOOGLE_CLIENT_ID,
   auth_uri: process.env.GOOGLE_AUTH_URI,
